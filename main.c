b04c899bea64a81792fbe4887205a09da18ccfff
Ormir G <ormirgj@gmail.com>
2016-05-12 12:33:03 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vehicle-test-grid
+//
+//  Created by Ormir G. on 12/05/16.
+//  Copyright © 2016 Lukas Auerbeck, Ormir Gjurgjej. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
