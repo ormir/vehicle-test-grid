1de1a41758f18be5f3fb5e9ecb8024eb49c4d7eb
Lukas Auerbeck <lauerbeck@gmx.at>
2016-05-17 20:23:05 +0200

    Revert "autocommit on change `date +%F-%T`"
    This reverts commit 44cd3d83f23ac2827022db0c66dce8ad09f089d6.

--- autocommit.sh
+++ autocommit.sh
@@ -7,8 +7,8 @@ git checkout auto-commit
 
 while true; do
 	git pull
-	fswatch -o ./*
-	git commit -a -m 'autocommit on change `date +%F-%T`'
-	git push 'auto-commit'
-	# sleep 2
+	inotifywait -qq -e CLOSE_WRITE ./*	
+	git commit -a -m 'autocommit on change'
+	git push origin 'auto-commit'
+	sleep 2
 done
