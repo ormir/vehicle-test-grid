58f68a1ca07e1ac6cbf18868b4e032ac75d10844
Lukas Auerbeck <lauerbeck@gmx.at>
2016-05-17 20:40:53 +0200

    autocommit on change

--- autocommit.sh
+++ autocommit.sh
@@ -7,14 +7,7 @@ git checkout auto-commit
 
 while true; do
 	git pull
-<<<<<<< HEAD
 	git commit -a -m 'autocommit on change'
 	git push origin 'auto-commit'
 	sleep 2
-=======
-	fswatch -o ./*
-	git commit -a -m 'autocommit on change `date +%F-%T`'
-	git push 'auto-commit'
-	# sleep 2
->>>>>>> master
 done
