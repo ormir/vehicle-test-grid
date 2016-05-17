44cd3d83f23ac2827022db0c66dce8ad09f089d6
Lukas Auerbeck <lauerbeck@gmx.at>
2016-05-17 20:21:23 +0200

    autocommit on change `date +%F-%T`

--- autocommit.sh
+++ autocommit.sh
@@ -7,8 +7,8 @@ git checkout auto-commit
 
 while true; do
 	git pull
-	inotifywait -qq -e CLOSE_WRITE ./*	
-	git commit -a -m 'autocommit on change'
-	git push origin 'auto-commit'
-	sleep 2
+	fswatch -o ./*
+	git commit -a -m 'autocommit on change `date +%F-%T`'
+	git push 'auto-commit'
+	# sleep 2
 done
