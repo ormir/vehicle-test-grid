41e5065679e63fd9e2f916dd7419ecbd6316b91d
Ormir G <ormirgj@gmail.com>
2016-05-18 17:45:16 +0200

    autocommit on change

--- vehicleclient.c
+++ vehicleclient.c
@@ -39,7 +39,7 @@ int main(int argc, char const *argv[]) {
 
 	// Signal Handling
 	if(signal(SIGTERM, signal_handler) == SIG_ERR) printf("\ncan't catch SIGTERM\n");
-	if(signal(SIGSEV, signal_handler) == SIG_ERR) printf("\ncan't catch SIGTERM\n");
+	if(signal(SIGSEGV, signal_handler) == SIG_ERR) printf("\ncan't catch SIGTERM\n");
 
 	// Get message queue
 	if((msgid = msgget(KEY, PERM)) == -1) {
