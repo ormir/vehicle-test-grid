8fde7ee807602b4cd55ee7892acbede04fb80c1b
Ormir G <ormirgj@gmail.com>
2016-05-18 18:49:53 +0200

    autocommit on change

--- griddisplay.c
+++ griddisplay.c
@@ -18,6 +18,7 @@ int main(int argc, char* argv[]) {
 
    }
 
+   // Read mesages from server
    while (1) {
 	   	if ((fp = fopen("display", "r")) != NULL) {
 		    while (fgets(puffer, x+1, fp) != NULL) {
